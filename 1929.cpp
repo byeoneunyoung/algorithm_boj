1f4e499fac80af3ae1c88f34703dac04840623e3
Eunyoung Byeon <ebyeon147@soongsil.ac.kr>
2023-01-09 11:27:36 +0900

    3트만에 성공

--- 1929.cpp
+++ 1929.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
 #include<cmath>
 //에라토스테네스의 채
-#define MAX 1000001
 using namespace std;
-void main(){
-    int m, n;
+ int m = 0, n = 0;
+ int pN[1000001] = {0, };
+int main() {
     cin>>m>>n;
-    int pN[MAX];
     for(int i = 2; i<=n; i++){
         pN[i] = i;
     }
-    for(int i = 2; i<sqrt(n); i++){
+    for(int i = 2; i*i<=n; i++){
         if(pN[i] == 0) continue;
         for(int j = i*i; j<=n; j+=i)
             pN[j] = 0;
@@ -18,4 +17,5 @@ void main(){
     for(int i = m; i<=n; i++){
         if(pN[i] != 0) cout<<pN[i]<<"\n";
     }
+    return 0;
 }
