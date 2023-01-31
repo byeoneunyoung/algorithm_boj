baf19dbd933e0d00fd1628f094c62515f0a80094
Eunyoung Byeon <ebyeon147@soongsil.ac.kr>
2023-01-31 20:16:34 +0900

    while 문 수정 후 재 채점- 맞음

--- 1167.cpp
+++ 1167.cpp
@@ -3,25 +3,39 @@
 #include<cstring>
 using namespace std;
 
+typedef struct{
+    int index;
+    int dist;
+}node;
+
 int m_d = 0, m_n = 0;
 bool visit[100001];
-vector<pair<int, int>> tree[100002];
+vector<node> tree[100002];
 //tree[a][0].first = b, tree[a][0].second = c면 
 //a에서 b로 가는 가중치 c를 가진 간선을 의미함
 
-int dfs(int, int);
+void dfs(int, int);
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int v;
     cin>>v;
     int a, b, c;
     for(int i = 0; i<v; i++){
         cin>>a;
-        while(c!=-1){
-            cin>>b>>c;
+        while(true){
+            cin>>b;
+            if(b == -1) break;
+            cin>>c;
+            
             tree[a].push_back({b, c});
+            tree[b].push_back({a, c});
         }
     }
+
     dfs(1,0);
     memset(visit, false, sizeof(visit));
     m_d = 0;
@@ -32,15 +46,15 @@ int main(){
 
 void dfs(int a, int c){
     if(visit[a]) return;
-    if(m<c){
+    if(m_d<c){
         m_d = c;
         m_n = a;
     }
     visit[a] = true;
 
     for(int i= 0; i< tree[a].size(); i++){
-        int nextIndex = tree[a][i].first;
-        int nextDist = tree[a][i].second;
-        dfs(nextIndex, nextDist+c);
+        int nextIndex = tree[a][i].index;
+        int nextDist = tree[a][i].dist;
+        dfs(nextIndex, nextDist + c);
     }
 }
